b823da31f385be16d6c2004aebb598756a224a3b
w01f6 <cristiancocioaba@gmail.com>
2014-03-26 14:19:11 +0200

    Update service_methods.php

--- service_methods.php
+++ service_methods.php
@@ -33,22 +33,30 @@ include 'My_API.php';
 							foreach ($method->getParameters() as $params)
 							{
 								echo '<div class="method-params">';
-									echo
-										'<div class="method-param-name">'
-											.'<label for="param-'.$params->name.$paramNr.'" class="label-param-name">'
-													.$params->name.':'.
-											'</label>';
+								//if method has 1 param it will accept array of params
 									if ($countParameters===1)
 									{
+										echo
+										'<div class="method-param-name">'
+											.'<label for="param-'.$params->name.$paramNr.'" class="label-param-name">'
+													.$params->name.':<br/>(array)'.
+											'</label>';
+									
 										echo '<div class="input-append ia-method-param-value">'
 												.'<input type="text" id="param-'.$params->name.$paramNr.'" 
 												class="method-param-value method-param-value'.$methodNr.'"/>'
 												  .'<button class="btn btn-small  btn-add-param-value" title="Add Value" 
-														type="button">+</button>'
+														type="button">Add</button>'
 											.'</div>';
 									}
+								//else fixed number of params
 									else
 									{
+										echo
+										'<div class="method-param-name">'
+											.'<label for="param-'.$params->name.$paramNr.'" class="label-param-name">'
+													.$params->name.':'.
+											'</label>';
 										echo '<input type="text" id="param-'.$params->name.$paramNr.'" 
 												class="method-param-value method-param-value'.$methodNr.'"/>';
 									}
@@ -58,24 +66,37 @@ include 'My_API.php';
 								$paramNr++;
 							}
 						}
-						//extra request TODO
+						//extra request -> it will be automatically to JSON and sent as request
 						echo '<div class="method-extra-request" id="method-extra-request'.$methodNr.'">'
 								.'<label for="extra-request'.$methodNr.'" class="label-extra-request">'
-											.'Request:'
+											.'Request Params:'
 											.'</label>'
-											.'<textarea  id="extra-request'.$methodNr.'" 
-												class="extra-request extra-request'.$methodNr.'"></textarea>'
+											//.'<textarea  id="extra-request'.$methodNr.'" 
+											//	class="extra-request extra-request'.$methodNr.'"></textarea>'
+											.'<div class="well container-extra-request" id="container-extra-request">'
+												.'<div class="container-extra-request-name">'
+													.'<label for="extra-request-param-name">Name:</label>'
+													.'<input type="text" id="extra-request-param-name"/>'
+												.'</div>'
+												.'<div class="container-extra-request-value">'
+													.'<label for="extra-request-param-value">Value:</label>'
+													.'<input type="text" id="extra-request-param-value"/>'
+												.'</div>'
+												//.'<button type="button" class="close close-parent">&times;</button>'
+												.'<button class="btn pull-right btn-small btn-add-extra-request " title="Add Request Params" 
+														type="button">Add</button>'
+											.'</div>'
 							.'</div>';
-						//buttons
+						//buttons for HTTP method of request
 						echo '<div class="method-buttons" id="method-buttons'.$methodNr.'" >'
-								.'<input type="hidden" id="uri-method'.$methodNr.'" 
+								.'<input type="hidden" class="inputMethodNr" id="uri-method'.$methodNr.'" 
 										value="'.$WS_ROOT.$URI.'">'
 								.'<div class="btn-group btn-group-ws-response pull-right">'
-									.'<button class="btn btn-small btn-ws-response btn-get-response"
+									.'<button class="btn btn-small  btn-ws-response btn-get-response"
 										id="btn-get-response'.$methodNr.'" methodNr="'.$methodNr.'">
-										Get
+										<b>Get</b>
 									</button>'
-									.'<button class="btn btn-small btn-ws-response btn-post-response"
+									.'<button class="btn btn-small  btn-ws-response btn-post-response"
 										id="btn-post-response'.$methodNr.'" methodNr="'.$methodNr.'">
 										Post
 									</button>'
@@ -83,7 +104,7 @@ include 'My_API.php';
 										id="btn-put-response'.$methodNr.'" methodNr="'.$methodNr.'">
 										Put
 									</button>'
-									.'<button class="btn btn-small btn-ws-response btn-delete-response"
+									.'<button class="btn btn-small btn-danger btn-ws-response btn-delete-response"
 										id="btn-delete-response'.$methodNr.'" methodNr="'.$methodNr.'">
 										Delete
 									</button>'
@@ -95,9 +116,15 @@ include 'My_API.php';
 			}
 		?>
 	</div>
-	<div class="container span4">
-		<label id="label-return-ws" for="text-return-ws" > <b>Return: </b></label>
-		<textarea class="span5" id="text-return-ws"></textarea>
+	<div class="container span5">
+	<button type="button" class="btn pull-right" id="resetAllInputs" 
+		onclick="ResetAllInputs();">
+			Reset All
+	</button>
+		<div class="container">
+			<label id="label-return-ws" for="text-return-ws" > <b>Return: </b></label>
+			<textarea class="span5" id="text-return-ws"></textarea>
+		</div>
 	</div>
 </div>
 
