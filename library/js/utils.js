7f9677eb70551caf2b9052dcf9af75c2e674c36c
w01f6 <cristiancocioaba@gmail.com>
2014-03-26 14:20:13 +0200

    Update utils.js

--- library/js/utils.js
+++ library/js/utils.js
@@ -809,3 +809,8 @@ function GetCurrentTime(roundToHalf){
      theString+=hour+":"+min;   
      return theString;
 }
+jQuery.fn.outerHTML = function(s) {
+    return s
+        ? this.before(s).remove()
+        : jQuery("<p>").append(this.eq(0).clone()).html();
+};
